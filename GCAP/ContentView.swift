ec895fe7075fb42665df70f66454c03e6b92a619
softwaredeveloper222 <elitemobilexx@gmail.com>
2026-07-10 10:51:58 -0500

    added some padding to main page

--- GCAP/ContentView.swift
+++ GCAP/ContentView.swift
@@ -72,6 +72,7 @@ struct ContentView: View {
                                 }
                             }
                             .padding()
+                            .padding(.bottom, 36)
                         }
                     }
                 }
